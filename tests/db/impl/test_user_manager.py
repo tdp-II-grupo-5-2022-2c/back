0f66540b9d6e83cb3a177a3225ec5464f1851880
Milagros Virginia Cruz <milagros.cruz@mercadolibre.com>
2022-09-16 08:35:15 -0300

    Unit test for get_user

--- tests/db/impl/test_user_manager.py
+++ tests/db/impl/test_user_manager.py
@@ -2,7 +2,9 @@ import unittest
 import pytest
 from unittest.mock import MagicMock
 
-from app.db.impl.user_manager import UserManager, UserModel
+from app.db.impl.user_manager import UserManager
+from app.db.model.user import UserModel
+from app.db.model.my_sticker import MyStickerModel
 from app.db.model.py_object_id import PyObjectId
 
 class TestUserManager(unittest.TestCase):
@@ -10,12 +12,18 @@ class TestUserManager(unittest.TestCase):
 
     @pytest.mark.asyncio
     async def test_get_user(self):
+        sticker = MyStickerModel(
+            id="sticker_id",
+            is_on_album=False,
+            quantity=2
+        )
         user = UserModel(
-            _id=PyObjectId("62be27922d98b5aaad951f95"),
-            user_id="user_id"
+            _id=PyObjectId("1"),
+            mail="usermail@gmail.com",
+            stickers=[]
         )
         self.db["users"].find_one = MagicMock(return_value=user)
 
         user_manager = UserManager(self.db)
-        result = await user_manager.get_by_id("62be27922d98b5aaad951f95")
+        result = await user_manager.get_by_id("1")
         self.assertIsNotNone(result)
