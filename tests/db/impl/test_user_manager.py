5bc95a80e82bc57065f569aafac92907010a4048
Elianagam <elianagam2@gmail.com>
2022-09-18 20:16:59 -0300

    add test

--- tests/db/impl/test_user_manager.py
+++ tests/db/impl/test_user_manager.py
@@ -5,6 +5,7 @@ from unittest.mock import MagicMock
 from app.db.impl.user_manager import UserManager
 from app.db.model.user import UserModel
 from app.db.model.my_sticker import MyStickerModel
+from app.db.model.package import PackageModel
 from app.db.model.py_object_id import PyObjectId
 
 
@@ -89,5 +90,49 @@ class TestUserManager(unittest.TestCase):
         self.assertEqual(True, result[0].is_on_album)
         self.assertEqual(1, result[0].quantity)
 
-        self.db["users"].update_one = MagicMock(return_value=True)
 
+    @pytest.mark.asyncio
+    async def test_open_package(self):
+        # Given
+        in_package = []
+        my_stickers = []
+        for i in range(5):
+            sticker = StickerModel(
+                _id=PyObjectId(f"{i}"),
+                image=f"img_{i}.png",
+                type="normal"
+            )
+            my_sticker = MyStickerModel(
+                _id=f"{i}",
+                quantity=1,
+                is_on_album=False
+            )
+            my_stickers.append(my_sticker)
+            in_package.append(sticker)
+
+        package = PackageModel(user_id="10", stickers=in_package)
+
+        user = UserModel(
+            _id=PyObjectId("2"),
+            mail="usermail@gmail.com",
+            stickers=my_sticker
+        )
+
+        self.db["users"].update_one = MagicMock(return_value=True)
+        self.db["users"].find_one = MagicMock(return_value=user)
+
+        user_manager = UserManager(self.db)
+
+        # When
+        result = await user_manager.open_package(package)
+
+        # Then
+        self.assertIsNotNone(result)
+        self.assertEqual(1, len(result))
+        self.assertEqual("10", result[0].id)
+        self.assertEqual(5, len(result.stickers[0]))
+        self.assertEqual("1", result.stickers[0].id)
+        self.assertEqual(1, result.stickers[0].quantity)
+        self.assertEqual(False, result.stickers[0].is_on_album)
+
+    
