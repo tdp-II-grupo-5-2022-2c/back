fd79b4d6e1f629e7b77b3ad4a53aaf692170ce61
Daniela Carrero <yriveros@amazon.com>
2022-09-19 18:41:12 -0300

    fix open packet

--- app/adapters/sticker_controller.py
+++ app/adapters/sticker_controller.py
@@ -30,9 +30,9 @@ async def get_daily_package(
     try:
         package = await manager.create_package()
         # After create package open package and add to user myStickers
-        response = await user_manager.open_package(package=package, user_id=user_id.id)
+        await user_manager.open_package(package=package, user_id=user_id.user_id)
         return JSONResponse(
-                status_code=status.HTTP_201_CREATED, content=jsonable_encoder(response)
+                status_code=status.HTTP_201_CREATED, content=jsonable_encoder(package)
             )
     except Exception as e:
         raise HTTPException(
