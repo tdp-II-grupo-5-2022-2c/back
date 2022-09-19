3196e04946fb6da4778b8b577975fc1db535959c
Elianagam <elianagam2@gmail.com>
2022-09-18 23:17:52 -0300

    Fix linter

--- app/adapters/dtos/user.py
+++ app/adapters/dtos/user.py
@@ -1,7 +1,6 @@
 from pydantic.main import BaseModel
 from typing import List
 from app.db.model.my_sticker import MyStickerModel
-from app.adapters.dtos.sticker_complete import StickerCompleteResponse
 
 
 class UserResponse(BaseModel):
