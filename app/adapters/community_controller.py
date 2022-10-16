8d92630f8d4f905f70e5f07662965ad51bb30560
Milagros Virginia Cruz <milagros.cruz@mercadolibre.com>
2022-10-16 11:30:52 -0300

    Get by owner and get by user working

--- app/adapters/community_controller.py
+++ app/adapters/community_controller.py
@@ -8,29 +8,29 @@ from app.db import DatabaseManager, get_database
 from app.db.impl.community_manager import CommunityManager
 from app.db.model.community import CommunityModel
 
-
 router = APIRouter(tags=["communities"])
 
 
 @router.get(
     "/communities",
-    response_description="Get all communities",
+    response_description="Get community by owner id or member id."
+                         "If no parameter is specified, it will get all communities",
     status_code=status.HTTP_200_OK,
 )
-async def get_all(
-    owner_id: str = None,
-    member_id: str = None,
-    db: DatabaseManager = Depends(get_database),
+async def get_communities(
+        owner: str = None,
+        member: str = None,
+        db: DatabaseManager = Depends(get_database),
 ):
     manager = CommunityManager(db.db)
     try:
-        response = await manager.get_all(owner_id, member_id)
+        response = await manager.get_communities(owner, member)
         return response
     except HTTPException as e:
         raise e
     except Exception as e:
         raise HTTPException(
-            status_code=500, detail=f"Error getting All Communities. Exception {e}"
+            status_code=500, detail=f"Error getting Communities. Exception {e}"
         )
 
 
@@ -40,8 +40,8 @@ async def get_all(
     status_code=status.HTTP_200_OK,
 )
 async def get_community_by_id(
-    community_id: str,
-    db: DatabaseManager = Depends(get_database),
+        community_id: str,
+        db: DatabaseManager = Depends(get_database),
 ):
     manager = CommunityManager(db.db)
     try:
@@ -70,8 +70,8 @@ async def create_community(
     try:
         response = await manager.add_new(community=community)
         return JSONResponse(
-                status_code=status.HTTP_201_CREATED, content=jsonable_encoder(response)
-            )
+            status_code=status.HTTP_201_CREATED, content=jsonable_encoder(response)
+        )
     except Exception as e:
         raise HTTPException(
             status_code=500, detail=f"Could not create Community. Exception: {e}"
