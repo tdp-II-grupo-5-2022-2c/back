2fbddbf73b644c6a290ab2e2cafc9758be9cc51a
Elianagam <elianagam2@gmail.com>
2022-10-23 18:55:58 -0300

    Add response

---
+++ app/adapters/dtos/community_details.py
@@ -0,0 +1,19 @@
+from pydantic.main import BaseModel
+from typing import List
+
+
+class UserNameResponse(BaseModel):
+    id: str
+    name: str
+    lastname: str
+    mail: str
+
+
+class CommunityDetailResponse(BaseModel):
+    id: str
+    name: str
+    owner: str
+    users: List[UserNameResponse]
+    password: str
+
+
