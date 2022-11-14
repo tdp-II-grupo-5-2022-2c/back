6a4f4a9f291ef5fa674d83d7408dbbbf9e5d784b
Elianagam <elianagam2@gmail.com>
2022-11-14 20:41:49 -0300

    fix to list

--- app/db/impl/community_manager.py
+++ app/db/impl/community_manager.py
@@ -49,11 +49,11 @@ class CommunityManager:
         return comms
 
     async def get_by_name(self, name: str):
-        comm = await self.db["communities"].find_one({"name": name}).to_list(5000)
+        comm = await self.db["communities"].find_one({"name": name})
         return comm
 
     async def get_blocked(self, blocked: bool):
-        comm = await self.db["communities"].find_one({"is_blocked": blocked}).to_list(5000)
+        comm = await self.db["communities"].find({"is_blocked": blocked}).to_list(5000)
         return comm
 
     async def get_community_by_id(self, community_id: str):
