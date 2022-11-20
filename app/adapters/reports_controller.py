ecb4da9671e17b44737d5cc962fff67778e6687c
Daniel Bizari <bizdanie@amazon.com>
2022-11-20 13:58:46 -0300

    render complete stickers info

--- app/adapters/reports_controller.py
+++ app/adapters/reports_controller.py
@@ -11,7 +11,7 @@ from app.db.impl.user_manager import UserManager, GetUserManager
 from app.db.model.sticker import StickerModel, UpdateStickerModel
 from app.db.model.sticker_metrics import StickerMetricsModel
 from app.db.model.user_id import UserIdModel
-from typing import List
+from typing import List, Dict
 
 router = APIRouter(tags=["reports"])
 
@@ -25,9 +25,21 @@ async def get_stickers(
         sticker_manager: StickerManager = Depends(GetStickerManager),
 ):
     try:
-        result = await sticker_manager.get_sticker_metrics_top_5()
+        top5 = await sticker_manager.get_sticker_metrics_top_5()
+        result = await renderStickers(top5)
         return result
     except Exception as e:
         raise HTTPException(
             status_code=500, detail=f"Unexpected error getting top 5. Exception: {e}"
         )
+
+
+async def renderStickers(stickers: List[Dict]) -> List[Dict]:
+    sticker_manager: StickerManager = await GetStickerManager()
+    result = []
+    for s in stickers:
+        sticker = await sticker_manager.get_by_id(s['sticker_id'])
+        sticker['counter'] = s['counter']
+        result.append(sticker)
+
+    return result
