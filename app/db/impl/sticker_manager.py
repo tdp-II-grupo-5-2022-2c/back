100839de739a9d8268f470ee37b9aef294c43a49
Milagros Virginia Cruz <milagros.cruz@mercadolibre.com>
2022-11-27 21:46:11 -0300

    random stickers when creating packages

--- app/db/impl/sticker_manager.py
+++ app/db/impl/sticker_manager.py
@@ -1,10 +1,12 @@
 import logging
+from random import choice
 
 from fastapi import Body
 from fastapi.encoders import jsonable_encoder
 from motor.motor_asyncio import AsyncIOMotorDatabase
 from typing import Union
 from app.db import DatabaseManager, get_database
+from app.db.impl.user_manager import TOTAL_STICKERS_ALBUM
 
 from app.db.model.package import PackageModel
 from app.db.model.package_counter import PackageCounterModel
@@ -13,6 +15,21 @@ from app.db.model.sticker_metrics import StickerMetricsModel
 from typing import List, Dict
 from fastapi_pagination.ext.motor import paginate
 from fastapi_pagination import Params
+
+
+def get_random_stickers_from_list(sticker_list: List[StickerModel], amount_of_stickers_desired: int):
+    random_sticker_list = []
+    amount_of_stickers_to_return = min(amount_of_stickers_desired, len(sticker_list))
+    positions_stickers_added = []
+
+    for i in range(amount_of_stickers_to_return):
+        random_pos = choice(
+            [number for number in range(0, len(sticker_list)) if number not in positions_stickers_added]
+        )
+        random_sticker_list.append(sticker_list[random_pos])
+        positions_stickers_added.append(random_pos)
+
+    return random_sticker_list
 
 
 class StickerManager:
@@ -75,7 +92,7 @@ class StickerManager:
 
     async def update_sticker_metrics(self, stickerMetrics: StickerMetricsModel):
         payload = {k: v for k, v in stickerMetrics.dict().items() if v is not None}
-        await self.db["stickers_metrics"].\
+        await self.db["stickers_metrics"]. \
             update_one({"sticker_id": stickerMetrics.sticker_id}, {"$set": payload})
 
     async def update(self, id: str, sticker: UpdateStickerModel = Body(...)):
@@ -94,19 +111,31 @@ class StickerManager:
             package_counter_model = PackageCounterModel(**package_counter)
             package_amount = package_counter_model.counter
 
-            if (package_amount % 11 != 0) or (package_amount == 0):
-                stickers_in_package = await self.db["stickers"].find({
-                    "weight": {"$gte": 1, "$lte": 3}
-                }).to_list(5)
+            # We get all the stickers whose weight is between 1 and 3
+            stickers_with_weight_1 = await self.db["stickers"].find({"weight": 1}).to_list(TOTAL_STICKERS_ALBUM)
+            stickers_with_weight_2 = await self.db["stickers"].find({"weight": 2}).to_list(TOTAL_STICKERS_ALBUM)
+            stickers_with_weight_3 = await self.db["stickers"].find({"weight": 3}).to_list(TOTAL_STICKERS_ALBUM)
 
-                i = 4
+            # If it is a normal package, we need 5 stickers whose weight is between 1 and 3
+            if (package_amount % 11 != 0) or (package_amount == 0):
+
+                # Select 5 random ones stickers whose weight is between 1 and 3
+                stickers_with_weight_1_to_3 = stickers_with_weight_1 + stickers_with_weight_2 + stickers_with_weight_3
+                stickers_in_package = get_random_stickers_from_list(stickers_with_weight_1_to_3, 5)
+
+                # Some variables that will help us with the amount of stickers check
+                next_sticker_weight_to_add = 4
                 stickers_remaining = []
                 stickers_up_to_now = stickers_in_package
-                while len(stickers_up_to_now) < 5 and i < 6:
+
+                # If the amount of stickers with weight between 1 and 3 is less than 5,
+                # we will be adding stickers with higher weights to the package
+                while len(stickers_up_to_now) < 5 and next_sticker_weight_to_add < 6:
                     stickers_remaining = await self.db["stickers"]. \
-                        find({"weight": {"$gte": 4, "$lte": i}}) \
-                        .to_list(5 - len(stickers_in_package))
-                    i += 1
+                        find({"weight": {"$gte": 4, "$lte": next_sticker_weight_to_add}}) \
+                        .to_list(TOTAL_STICKERS_ALBUM)
+                    stickers_remaining = get_random_stickers_from_list(stickers_remaining, 5 - len(stickers_in_package))
+                    next_sticker_weight_to_add += 1
                     stickers_up_to_now = stickers_in_package + stickers_remaining
 
                 stickers_in_package = stickers_in_package + stickers_remaining
@@ -114,22 +143,46 @@ class StickerManager:
                 if len(stickers_in_package) < 5:
                     raise Exception("No stickers at the moment to create a package")
             else:
-                difficult_sticker = await self.db["stickers"].find({"weight": 5}).to_list(1)
-                i = 4
-                while len(difficult_sticker) < 1 and i > 0:
-                    difficult_sticker = await self.db["stickers"].find({"weight": i}).to_list(1)
-                    i -= 1
+                # If it is a "special package", we need:
+                # 1 sticker with weight 5
+                # 2 stickers with weight 1
+                # 2 stickers with weight between 2 and 4
+
+                # Get the remaining stickers lists that we will use
+                stickers_with_weight_4 = await self.db["stickers"].find({"weight": 4}).to_list(TOTAL_STICKERS_ALBUM)
+                stickers_with_weight_5 = await self.db["stickers"].find({"weight": 5}).to_list(TOTAL_STICKERS_ALBUM)
+
+                # Search for a sticker with weight 5
+                difficult_sticker = get_random_stickers_from_list(stickers_with_weight_5, 1)
+
+                # Variable that will help with the amount of stickers check
+                next_sticker_weight_to_add = 4
+
+                # If there is no sticker with weight 5, we search one with a lower weight
+                while len(difficult_sticker) < 1 and next_sticker_weight_to_add > 0:
+                    difficult_sticker = await self.db["stickers"].find({"weight": next_sticker_weight_to_add}).to_list(
+                        1)
+                    next_sticker_weight_to_add -= 1
+
                 if len(difficult_sticker) < 1:
                     raise Exception("No stickers at the moment to create a package")
 
-                easy_stickers = await self.db["stickers"].find({"weight": 1}).to_list(2)
+                # Select two stickers with weight 1
+                easy_stickers = get_random_stickers_from_list(stickers_with_weight_1, 2)
+
+                # Variables that will help with the amount of stickers check
                 easy_stickers_remaining = []
                 easy_stickers_up_to_now = easy_stickers
-                i = 2
-                while len(easy_stickers_up_to_now) < 2 and i < 6:
-                    easy_stickers_remaining = await self.db["stickers"].find({"weight": i}) \
-                        .to_list(2 - len(easy_stickers))
-                    i += 1
+                next_sticker_weight_to_add = 2
+
+                # If the amount of stickers with weight 1 is less than 2,
+                # we search for stickers with higher weight
+                while len(easy_stickers_up_to_now) < 2 and next_sticker_weight_to_add < 6:
+                    easy_stickers_remaining = await self.db["stickers"].find({"weight": next_sticker_weight_to_add}) \
+                        .to_list(TOTAL_STICKERS_ALBUM)
+                    easy_stickers_remaining = get_random_stickers_from_list(easy_stickers_remaining,
+                                                                            2 - len(easy_stickers))
+                    next_sticker_weight_to_add += 1
                     easy_stickers_up_to_now = easy_stickers_up_to_now + easy_stickers_remaining
 
                 easy_stickers = easy_stickers + easy_stickers_remaining
@@ -137,19 +190,25 @@ class StickerManager:
                 if len(easy_stickers) < 2:
                     raise Exception("No stickers at the moment to create a package")
 
-                medium_stickers = await self.db["stickers"].find({
-                    "weight": {"$gte": 2, "$lte": 4}
-                }).to_list(2)
+                # We search for 2 stickers with weight between 2 and 4
+                stickers_with_weight_2_to_4 = stickers_with_weight_2 + stickers_with_weight_3 + stickers_with_weight_4
+                medium_stickers = get_random_stickers_from_list(
+                    stickers_with_weight_2_to_4, 2)
+
+                # If the amount of stickers with weight between 2 and 4 is less than 2,
+                # we search for stickers with weight 1
                 if len(medium_stickers) < 2:
-                    remaining_medium_stickers = await self.db["stickers"].find({
-                        "weight": 1
-                    }).to_list(2 - len(medium_stickers))
+                    remaining_medium_stickers = get_random_stickers_from_list(stickers_with_weight_1,
+                                                                              2 - len(medium_stickers))
                     medium_stickers = medium_stickers + remaining_medium_stickers
+
+                # If the amount of stickers with weight between 1 and 4 is less than 2,
+                # we search for stickers with weight 5
                 if len(medium_stickers) < 2:
-                    remaining_medium_stickers = await self.db["stickers"].find({
-                        "weight": 5
-                    }).to_list(2 - len(medium_stickers))
+                    remaining_medium_stickers = get_random_stickers_from_list(stickers_with_weight_5,
+                                                                              2 - len(medium_stickers))
                     medium_stickers = medium_stickers + remaining_medium_stickers
+
                 if len(medium_stickers) < 2:
                     raise Exception("No stickers at the moment to create a package")
 
